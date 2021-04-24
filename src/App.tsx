6ef2324985b5e4924bc658915d5b7050f6cb0645
sara-ahrari <sara.ahrari97@gmail.com>
2021-04-24 14:15:53 +0200

    firts commit2

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-                <GlobalStyle/>
+                      <GlobalStyle/>
       <header className="App-header">
       </header>
 
