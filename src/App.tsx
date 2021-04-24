94f8d9b784bd9bdb7a2c7c60f528e3b1c70fb9df
sara-ahrari <sara.ahrari97@gmail.com>
2021-04-24 14:12:37 +0200

    firts commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import GlobalStyle from './styles/Global.styles';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle/>
+      <header className="App-header">
+      </header>
+
+      <div className="text-container">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        </div>
+    </div>
+  );
+}
+
+export default App;
